e932aa06e99611be08f755c7de012f00e8d05faa
Max Smoot <max.smoot@sonos.com>
2023-01-23 13:46:46 -0500

    Initial Commit

---
+++ Exploration/ExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorationApp.swift
+//  Exploration
+//
+//  Created by Max Smoot on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
