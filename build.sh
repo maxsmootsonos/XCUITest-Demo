2aff482d075b6d30d47fddea5905fc9f16e3771c
Max Smoot <max.smoot@sonos.com>
2023-02-02 12:08:10 -0500

    added jenkinsfile

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 xcodebuild clean && \
 xcodebuild \
--scheme Exploration
--sdk iphonesimulator
--destination 'platform=iOS Simulator,name=iPhone 14' 
+-scheme Exploration \
+-sdk iphonesimulator \
+-destination 'platform=iOS Simulator,name=iPhone 14' \
 test \
 | tee xcodebuild.log && \
 /opt/homebrew/bin/xcbeautify --report-path /test-reports --report junit < xcodebuild.log
